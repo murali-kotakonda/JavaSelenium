01b9ef4378948ace132bfe8949b07b3ddff4afc2
Muralidhar <mythritechsolutions@gmail.com>
2019-01-20 18:13:04 +0530

    Add files via upload

---
+++ TestNavigation1.java
@@ -0,0 +1,36 @@
+package com.mythri;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class TestNavigation1 extends BaseTest{
+
+	@Test
+	public void test() throws InterruptedException {
+		
+
+		driver.get("http:\\www.gmail.com");
+
+		driver.navigate().to("http://www.google.co.in");
+
+
+		driver.navigate().back();// visites gmail
+
+
+		driver.navigate().forward(); // google
+
+
+		driver.navigate().refresh(); // refresh google
+		
+		String currentUrl = driver.getCurrentUrl();
+		System.out.println(currentUrl);
+
+		Assert.assertEquals("Expected google url",true,currentUrl.contains("www.google.co.in"));
+
+		String title = driver.getTitle();
+		Assert.assertEquals("Expected google as title","Google India",title);
+		
+	}
+	
+
+}
